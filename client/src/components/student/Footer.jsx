5037880a54a26e79970ee517ceba45d323616a90
shubhamchawla1 <shubham07843@gmail.com>
2025-06-05 14:01:21 -0500

    Footer line change

--- client/src/components/student/Footer.jsx
+++ client/src/components/student/Footer.jsx
@@ -9,8 +9,8 @@ const Footer = () => {
         <div className="flex flex-col md:items-start items-center w-full">
           <img src={assets.logo_dark} alt="logo" />
           <p className="mt-6 text-center md:text-left text-sm text-white/80">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.
-          </p>
+          CodeMentorHub is a powerful LMS platform for creating, managing, and learning tech skills online. It connects mentors and students through interactive courses, projects, and real-time feedback.
+          </p> 
         </div>
 
         <div className="flex flex-col md:items-start items-center w-full">
