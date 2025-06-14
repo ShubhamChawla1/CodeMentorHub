9d917b7a67b6f449f383ddb525e9a40307132b4b
shubhamchawla1 <shubham07843@gmail.com>
2025-06-14 02:26:14 -0500

    TRY

--- server/routes/courseRoute.js
+++ server/routes/courseRoute.js
@@ -1,14 +1,18 @@
-import express from 'express'
-import { getAllCourse, getCourseId } from '../controllers/courseController.js';
+import express from "express";
+import {
+  getAllCourse,
+  getCourseId,
+  getPendingCourses,
+} from "../controllers/courseController.js";
 
+const courseRouter = express.Router();
 
-const courseRouter = express.Router()
-
+// Get Pending Courses
+courseRouter.get("/pending", getPendingCourses);
 // Get All Course
-courseRouter.get('/all', getAllCourse)
+courseRouter.get("/all", getAllCourse);
 
 // Get Course Data By Id
-courseRouter.get('/:id', getCourseId)
-
+courseRouter.get("/:id", getCourseId);
 
 export default courseRouter;
