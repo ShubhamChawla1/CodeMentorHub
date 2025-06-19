bb50cc7d8755f4a15e78442c90ed7dbaf7661611
shubhamchawla1 <shubham07843@gmail.com>
2025-06-18 23:06:43 -0500

    commit

---
+++ client/src/pages/student/Aboutus.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center p-8">
+      <h1 className="text-3xl font-bold mb-4">About Us</h1>
+      <p className="text-lg text-gray-600">
+        Welcome to our platform! We're a team of educators and developers passionate about creating a seamless learning experience.
+      </p>
+      <p className="text-lg text-gray-600">
+        Our mission is to provide high-quality courses and resources to help students achieve their goals. We're committed to making education accessible and enjoyable for everyone.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
