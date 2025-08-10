c3741da1fd27458cbe76c3718c1d2a99b9cdd7b2
shubhamchawla1 <shubham07843@gmail.com>
2025-08-10 18:01:58 -0500

    change Footer

--- client/src/components/educator/Footer.jsx
+++ client/src/components/educator/Footer.jsx
@@ -5,20 +5,20 @@ const Footer = () => {
   return (
     <footer className="flex md:flex-row flex-col-reverse items-center justify-between text-left w-full px-8 border-t">
       <div className='flex items-center gap-4'>
-        <img className='hidden md:block w-20' src={assets.logo} alt="logo" />
+        <a href="#"><img className='hidden md:block w-20' src={assets.logo} alt="logo" /></a>
         <div className='hidden md:block h-7 w-px bg-gray-500/60'></div>
         <p className="py-4 text-center text-xs md:text-sm text-gray-500">
           Copyright 2025 © CodeMentorHub. All Right Reserved.
         </p>
       </div>
       <div className='flex items-center gap-3 max-md:mt-4'>
-        <a href="#">
+        <a href="https://www.Facebook.com" target='_blank'>
           <img src={assets.facebook_icon} alt="facebook_icon" />
         </a>
-        <a href="#">
+        <a href="https://www.twitter.com" target='_blank'>
           <img src={assets.twitter_icon} alt="twitter_icon" />
         </a>
-        <a href="#">
+        <a href="https://www.instagram.com" target='_blank'>
           <img src={assets.instagram_icon} alt="instagram_icon" />
         </a>
       </div>
